38cc2c2f8f26a2769c91ed7c1a73f78e5d9ace67
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-08 22:48:37 +0300

    begin extraction

--- app/routers/ask.py
+++ app/routers/ask.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, UploadFile, File
 router = APIRouter(prefix="/ask")
 
 @router.get("/")
 async def ask():
     return "hello world"
+@router.post("/summarize-cv")
+async def get_summary(file: UploadFile = File(...)):
+    contents = await file.read()
+    return {"filename": len(contents)}
---
+++ app/services/extraction_service.py
@@ -0,0 +1,3 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
