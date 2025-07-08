619bf6df0b859f1bbbebfb8accc563e389944c45
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-08 18:18:06 +0300

    backend baby

--- frontend/src/app/cvsummary/page.tsx
+++ frontend/src/app/cvsummary/page.tsx
@@ -10,7 +10,7 @@ export default function cvsummary(){
                 <div className="flex flex-col bg-white p-2 rounded-lg">
                     Upload your CV
                     <div className="m-2 h-100 bg-stone-100 rounded-lg w-100">
-
+                        {/*render the first page of pdf here bro */}
                     </div>
                     <div className="m-2 border border-pink-500 p-1 rounded-lg flex items-center justify-center">
                         <label htmlFor="cv-upload" className=" text-pink-500">
